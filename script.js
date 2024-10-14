e5e76b3782e93f632e474abe4cf03976fb3f6e37
curseb100d <libyahan123@gmail.com>
2024-10-14 12:23:15 +0800

    Brat 1.3.0

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-window.addEventListener('keydown', function(e) {
+function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.bratkey[data-key="${e.keyCode}"]`);
     if(!audio) return;
     audio.currentTime = 0; //I just want to rewind to the start;
     audio.play(); //Play the keyboard sound
-    key.classList.add('playing');
-});
-
-function removeTransition(e) {
-    console.log(e);
+    key.classList.add('bratplaying');
 }
 
-const keys = document.querySelectorAll('.key');
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; // skip it if it's not a transform
+    this.classList.remove('bratplaying');
+}
+
+const keys = document.querySelectorAll('.bratkey');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
